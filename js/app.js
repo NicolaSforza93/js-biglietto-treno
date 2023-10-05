7a6f22030805aaa2575d65c684011e8cd7e52552
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-05 10:39:51 +0200

    added fix file js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // - chiedere all'utente il numero di chilometri che vuole percorrere
 //   - dichiarare una variabile e assegnarle il valore restituito da un prompt
-const numKilometers = prompt('Quanti chilometri devi percorrere?')
+const numKilometers = parseFloat(prompt('Quanti chilometri devi percorrere?'))
 console.log(numKilometers)
 // - chiedere all'utente l'età
 //   - dichiarare una variabile e assegnarle il valore restituito da un prompt
@@ -12,20 +12,21 @@ let basePrice = numKilometers * 0.21
 console.log(basePrice)
 // - calcolare il prezzo totale del viaggio 
 //   - applicare lo sconto
+let discount = 0
 if (userAge < 18 ) {
 //     - SE l'utente è < di 18 anni 
 //       - l'utente avrà uno sconto del 20%
     let discount20 = basePrice * 20 / 100
     console.log(discount20)
-    basePrice = basePrice - discount20
 } else if (userAge > 65) {
 //     - ALTRIMENTI SE l'utente è > di 65 anni
 //       - l'utente avrà uno sconto del 40%
     let discount40 = basePrice * 40 / 100
     console.log(discount40)
-    basePrice = basePrice - discount40
 } 
+
+const finalPrice = basePrice - discount
 //     - ALTRIMENTI il prezzo sarà quello base
  // - stampare il prezzo nella pagina 
  const priceDomElement = document.getElementById('final-price')
- priceDomElement.innerHTML = basePrice.toFixed(2)
+ priceDomElement.innerHTML = finalPrice.toFixed(2)
